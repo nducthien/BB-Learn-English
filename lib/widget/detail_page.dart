0e89cd76085937b1b1bc739809709813f78f5a5d
nducthien <thien.chotrax23@gmail.com>
2021-03-05 11:34:04 +0700

    - create popup menu item

--- lib/widget/detail_page.dart
+++ lib/widget/detail_page.dart
@@ -21,7 +21,47 @@ class DetailPage extends StatelessWidget {
             },
           ),
           IconButton(icon: Icon(Icons.favorite), onPressed: () {}),
-          IconButton(icon: Icon(Icons.more_vert), onPressed: () {})
+          Padding(
+            padding: EdgeInsets.only(right: 10.0),
+            child: PopupMenuButton(
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  child: Row(
+                    children: [
+                      Icon(Icons.playlist_add, color: Colors.black45),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                        child: Text('Add to play list'),
+                      ),
+                    ],
+                  ),
+                ),
+                PopupMenuItem(
+                  child: Row(
+                    children: [
+                      Icon(Icons.download_rounded, color: Colors.black45),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                        child: Text('Download'),
+                      ),
+                    ],
+                  ),
+                ),
+                PopupMenuItem(
+                  child: Row(
+                    children: [
+                      Icon(Icons.share, color: Colors.black45),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                        child: Text('Share'),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+              child: Icon(Icons.more_vert),
+            ),
+          )
         ],
       ),
       body: DarkLightTheme(),
